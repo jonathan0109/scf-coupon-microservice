b9f70f32dfda9e4da067a9e86b1e449250d2402b
Jonathan Martínez Hernández <jonathanmh0109@gmail.com>
2020-04-01 17:20:07 -0600

    controller - coupon request body defined

--- scf-coupon-microservice/src/main/java/mx/unnamed/scf/controller/CouponRestController.java
+++ scf-coupon-microservice/src/main/java/mx/unnamed/scf/controller/CouponRestController.java
@@ -1,6 +1,7 @@
 package mx.unnamed.scf.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,7 +25,7 @@ public class CouponRestController {
 	
 	@RequestMapping(value = "/coupons/{code}", 
 			method = RequestMethod.GET)
-	public Coupon getCoupon(String code) {
+	public Coupon getCoupon(@PathVariable("code") String code) {
 		
 		return couponRepository.findByCode(code);
 	}
