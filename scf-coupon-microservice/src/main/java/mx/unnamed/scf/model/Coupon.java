a92aa72cf81afeb5a5f5159b29336ed5e4b7f904
Jonathan Martínez Hernández <jonathanmh0109@gmail.com>
2020-04-02 00:16:06 -0600

    model - coupon refactor, datasource configuration refactor

--- scf-coupon-microservice/src/main/java/mx/unnamed/scf/model/Coupon.java
+++ scf-coupon-microservice/src/main/java/mx/unnamed/scf/model/Coupon.java
@@ -5,10 +5,12 @@ import java.math.BigDecimal;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Coupon {
 
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String code;
